3c52e7065d02539a7088d2a673ac04a7e71646c1
Kyle Conroy <kyle.j.conroy@gmail.com>
2013-07-05 19:06:11 -0700

    Don't install a userlist

--- converge.sh
+++ converge.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pushd /vagrant
-./stack postgresql/manifest.json redis/manifest.json python/manifest.json
+./stack postgresql/manifest.json redis/manifest.json \
+	python/manifest.json
 
 # DEVELOPMENT
 sh installgo.sh
