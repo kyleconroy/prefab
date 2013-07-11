c63dbad5913f0872f21b0e19a508257b329c7cf1
Brandon Liu <bdon@bdon.org>
2013-07-10 18:25:19 -0700

    add nginx.

--- scripts/converge.sh
+++ scripts/converge.sh
@@ -4,7 +4,7 @@ set -x
 pushd /vagrant/software
 
 ./../stack postgresql/manifest.json redis/manifest.json \
-	python/manifest.json java/manifest.json rabbitmq/manifest.json
+	python/manifest.json java/manifest.json rabbitmq/manifest.json nginx/manifest.json
 
 # DEVELOPMENT
 popd
