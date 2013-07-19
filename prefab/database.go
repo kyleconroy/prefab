b6d9e23c32762cc0f5dc01259d81832e0b504766
Brandon Liu <bdon@bdon.org>
2013-07-19 00:36:44 -0700

    Fix database extensions.

--- prefab/database.go
+++ prefab/database.go
@@ -17,7 +17,6 @@ type Database struct {
 
 func (d Database) Create() error {
 	log.Println("Create database:", d.Name)
-	log.Println("Extensions: ", d.Extensions)
 
 	_, err := exec.Command("sudo", "-u", "postgres", "createdb", d.Name).CombinedOutput()
 
@@ -26,8 +25,8 @@ func (d Database) Create() error {
 	}
 
 	for _, extension := range d.Extensions {
-		sql_command := fmt.Sprintf("create extension %s", extension.Name)
-		_, err := exec.Command("psql", d.Name, "-c", sql_command).CombinedOutput()
+		sql_command := fmt.Sprintf("psql %s -c 'create extension %s'", d.Name, extension.Name)
+		_, err := exec.Command("su", "postgres", "-c", sql_command).CombinedOutput()
 		if err != nil {
 			log.Println("Extension exists")
 		}
